d73e702734a23d93f028749813dd635c7b601ebb
roflan635 <nikolaev.nikita2281337@gmail.com>
2024-12-11 12:30:46 +0400

    изи

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -3,11 +3,38 @@
 
 #include <iostream>
 #include "Task.h"
+#include <iostream>
+
+void PrintMatrix(int** arr, int cols, int rows)
+{
+	for (int j = 0; j < rows; j++)
+	{
+		for (int i = 0; i < cols; i++)
+			std::cout << arr[j][i] << ' ';
+		std::cout << std::endl;
+	}
+}
+
+void FillMatrix(int** arr, int cols, int rows)
+{
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			std::cin >> arr[i][j];
+}
+
+int** CreateAndInitializeMatrix(int cols, int rows)
+{
+	if (rows <= 0 || cols <= 0) return nullptr; //если неверное кол-во строк и столбцов - выходим
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+		arr[i] = new int[cols];
+	FillMatrix(arr, cols, rows);
+	PrintMatrix(arr, cols, rows);
+	return arr;
+}
 
 int main()
-
 {
-	// Здесь должен быть ваш код
-
+	CreateAndInitializeMatrix(3, 3);
 }
 
